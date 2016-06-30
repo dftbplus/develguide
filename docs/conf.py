06de2065384151cdba2e3fe4fb4767229cff0bac
Bálint Aradi <baradi09@gmail.com>
2016-06-30 13:06:12 +0200

    Minor corrections in conf.py

--- docs/conf.py
+++ docs/conf.py
@@ -46,7 +46,7 @@ source_suffix = '.rst'
 master_doc = 'index'
 
 # General information about the project.
-project = 'Developers guide'
+project = 'DFTB+ Developers guide'
 copyright = '2016, DFTB+ developers'
 author = 'DFTB+ developers'
 
