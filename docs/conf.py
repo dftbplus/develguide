01194ffc29926d156522c94bf6338c50d75dc461
Tammo van der Heide <vanderhe@uni-bremen.de>
2024-02-15 10:58:48 +0100

    Fix theme

--- docs/conf.py
+++ docs/conf.py
@@ -47,7 +47,7 @@ master_doc = 'index'
 
 # General information about the project.
 project = 'DFTB+ Developers guide'
-copyright = '2016, DFTB+ developers'
+copyright = '2024, DFTB+ developers'
 author = 'DFTB+ developers'
 
 # The version info for the project you're documenting, acts as replacement for
@@ -110,7 +110,7 @@ todo_include_todos = False
 
 # The theme to use for HTML and HTML Help pages.  See the documentation for
 # a list of builtin themes.
-html_theme = 'default'
+html_theme = 'sphinx_book_theme'
 
 # Theme options are theme-specific and customize the look and feel of a theme
 # further.  For a list of options available for each theme, see the
